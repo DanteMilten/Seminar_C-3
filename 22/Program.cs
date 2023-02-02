58568fe7dcbd59e629af56ecaac119e6f78800bd
DanteMilten <milten47@yahoo.com>
2023-02-02 21:32:02 +0300

    22

---
+++ 22/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №22. Работа в группах
+// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+// ● 5 -> 1, 4, 9, 16, 25.
+// ● 2 -> 1,4
+
+// Console.Clear();
+// int number = ReadInt("Введите число: ");
+
+// for (int i = 1; i <= number; i++)
+// {
+//     Console.Write($"{i * i}, ");
+// }
+
+// // Метод
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
