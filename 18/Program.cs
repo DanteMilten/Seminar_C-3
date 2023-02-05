ae28faf441cb51d3ba5c29fbdf46de01f9383527
DanteMilten <milten47@yahoo.com>
2023-02-05 21:01:02 +0300

    18

--- 18/Program.cs
+++ 18/Program.cs
@@ -3,15 +3,35 @@
 // показывает диапазон возможных координат точек в этой четверти (x и y).
 
 Console.Clear();
-a = int(input('Введите номер четверти в которой бы хотели узнать диапозон возможных координат: '));
+Console.Write("Введите номер четверти ");
+int quarter = int.Parse(Console.ReadLine());
 
-if a == 1;
-    print('В первой четверти - x > 0 y > 0');
-else if a == 2;
-    print('Во второй четверти - x < 0 y > 0');
-else if a == 3;
-    print('В третьей четверти - x < 0 y < 0');
-else if a == 4;
-    print('В четвертой четверти - x > 0 y < 0');
-else
-    print('Такой четверти нет :(');
+switch (quarter)
+{
+    case 1:
+        {
+            Console.WriteLine("x>0 y>0");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("x<0 y>0");
+            break;
+        }
+     case 3:
+     {
+        Console.WriteLine("x<0 y<0");
+            break;
+     }   
+     case 4:
+     {
+        Console.WriteLine("x>0 y<0");
+            break;
+     }
+
+    default:
+        {
+            Console.WriteLine("Введена неправильная четверть");
+            break;
+        }
+}
