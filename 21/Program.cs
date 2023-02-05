1311f43f40b766cf108885eb18f2e2a2d757ae52
DanteMilten <milten47@yahoo.com>
2023-02-05 20:45:16 +0300

    21

--- 21/Program.cs
+++ 21/Program.cs
@@ -1,18 +1,27 @@
 ﻿// Напишите программу, 
 // которая принимает на вход координаты двух точек и находит 
 // расстояние между ними в 2D пространстве.
+
 // A (3,6); B(2, 1)-> 5,09
 // A(7, -5); B(1, -1)-> 7,21
-
 //sqrt((3 - 2)^2 + (6 - 1)^2) = 5.09
 //double s = Math.Sqrt(...);
 
+
 Console.Clear();
-Console.Write("Введите X: ");
-int X = int.Parse(Console.ReadLine());
+Console.Write("Введите X1: ");
+int x1 = int.Parse(Console.ReadLine());
+double X1 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+double Y1 = double.Parse(Console.ReadLine());
+Console.Write("Введите X2: ");
+int x2 = int.Parse(Console.ReadLine());
+double X2 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+int y2 = int.Parse(Console.ReadLine());
+double Y2 = double.Parse(Console.ReadLine());
 
-Console.Write("Введите Y: ");
-int Y = int.Parse(Console.ReadLine());
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
 
-double s = math.sqrt((ax-bx)**2+(ay-by)**2)
-
+Console.WriteLine($"d={d:f3}");
